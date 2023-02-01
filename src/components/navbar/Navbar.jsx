887b55283aa2c3319e36f267f687e20efd615bee
McMillan <117737074+BootCodex@users.noreply.github.com>
2023-02-01 20:16:20 +0200

    added css, assets and navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { RiMenu3Lines, RiCloseLine } from 'react-icons/ri' ;
+import logo from '../../assets/logo.svg';
 import './navbar.css';
+
+// BEM -> Block Element Modifier
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Navbar</h1>
+    <div className="gpt__navbar">
+      <div className="gpt__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo"/>
+        </div>
+      </div>
     </div>
   )
 }
