82366e6f011165a2801807aafc5a1e5457fa0f0d
McMillan <117737074+BootCodex@users.noreply.github.com>
2023-02-02 20:04:22 +0200

    navbar  links and button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { RiMenu3Lines, RiCloseLine } from 'react-icons/ri' ;
 import logo from '../../assets/logo.svg';
 import './navbar.css';
@@ -6,13 +6,26 @@ import './navbar.css';
 // BEM -> Block Element Modifier
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState()
   return (
-    <div className="gpt__navbar">
-      <div className="gpt__navbar-links">
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo"/>
         </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href='#Home'>Home</a></p>
+          <p><a href='#wgpt3'>What is GPT3</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
+        </div>
       </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign In</p>
+        <button type='button'>Sign Up</button>
+      </div>
+      <div className='gpt3__navbar-menu'></div>
     </div>
   )
 }
